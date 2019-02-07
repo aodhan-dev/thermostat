ed28a28c71b7e46af9b0f9b46409a45ebdb0c59e
Aiden Gleave <aidengleave@gmail.com>
2019-02-07 22:13:27 +0000

    Added increase temperature functionality - passing the test

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,4 +2,12 @@
 
 function Thermostat() {
   this.temperature = 20;
-}
+};
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
+};
