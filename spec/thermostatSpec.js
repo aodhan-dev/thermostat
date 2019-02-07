8eb4418c25549227332739d24662ed242c68a2b0
Aiden Gleave <aidengleave@gmail.com>
2019-02-07 22:39:00 +0000

    Wrote test for power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -28,5 +28,9 @@ describe('Thermostat', function() {
     }
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
+
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toEqual(true);
+  })
   
 });
