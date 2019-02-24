a9e1ec6d833e368a289b4ab531b68534295c315d
Aiden Gleave <aidengleave@gmail.com>
2019-02-24 17:00:04 +0000

    Refactored to add Default Temperature constant

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,8 @@
 'use strict'
 
 function Thermostat () {
-  this.temperature = 20
+  this.DEFAULT_TEMPERATURE = 20
+  this.temperature = this.DEFAULT_TEMPERATURE
   this.MINIMUM_TEMPERATURE = 10
   this.MAX_LIMIT_PSM_ON = 25
   this.MAX_LIMIT_PSM_OFF = 32
@@ -50,5 +51,5 @@ Thermostat.prototype.switchPowerSavingModeOn = function () {
 }
 
 Thermostat.prototype.resetTemperature = function () {
-  this.temperature = 20
+  this.temperature = this.DEFAULT_TEMPERATURE
 }
