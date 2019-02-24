9d2b552c678235d75494cd9900100aa090e92d64
Aiden Gleave <aidengleave@gmail.com>
2019-02-24 22:45:52 +0000

    Terrible Commit! - Adding API functionality.

--- src/interface.js
+++ src/interface.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   var thermostat = new Thermostat()
-  updateTemperature()
-  
+
   $('#temperature-up').click(function() {
     thermostat.up()
     updateTemperature()
@@ -31,4 +30,15 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature)
     $('#temperature').attr('class', thermostat.energyUsage())
   }
-})
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + 'a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data){
+        $('#current-temperature').text(data.main.temp);
+    })
+  })
+  
